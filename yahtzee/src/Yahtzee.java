be993e5157777952539dd9633f12695571c3cb70
Kaustubh <65749340+Kaustubh-Pasu@users.noreply.github.com>
2026-09-16 16:09:27 -0400

    start and mainMenu methods are now implemented in Yahtzee.java. The main method in Main.java now calls the start method in Yahtzee.java.

--- yahtzee/src/Yahtzee.java
+++ yahtzee/src/Yahtzee.java
@@ -1,5 +1,42 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class Yahtzee {
-    public static void say(String message) {
-        System.out.println("Yahtzee Says: \"" + message + "\"");
+    private List<Player> players;
+    private DiceSet dice;
+
+    public Yahtzee() {
+        this.dice = new DiceSet();
+    }
+
+    public void start() {
+        boolean running = true;
+        while (running) {
+            int choice = mainMenu();
+            //change input when OptionInput is implemented 
+            if (choice == 1){ 
+                gameSetup();
+                gameLoop();
+            } else {
+                running = false;
+            }
+        }
+    }
+
+    private int mainMenu() {
+        List<String> options = Arrays.asList("Play", "Quit");
+        return new OptionInput("Choose an option", options).get();
+    }
+
+    private void gameSetup() {
+
+    }
+
+    private void gameLoop() {
+
+    }
+
+    private boolean gameComplete() {
+        return false;
     }
 }
