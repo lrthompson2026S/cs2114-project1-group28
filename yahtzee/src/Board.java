f6b09386126430bc9b258675d4b8ef448028223b
Lucas Thompson <21lrthompson04@gmail.com>
2026-09-16 15:35:32 -0400

    Create basic class structure of Board

--- yahtzee/src/Board.java
+++ yahtzee/src/Board.java
@@ -1,3 +1,29 @@
 public class Board {
-    
+    public interface Section {}
+
+    public class Upper {
+        public enum Section implements Board.Section {
+            Aces, 
+            Twos, 
+            Threes,
+            Fours,
+            Fives,
+            Sixes
+        }
+    }
+
+    public class Lower {
+        public enum Section implements Board.Section {
+            ThreeOfKind,
+            FourOfKind,
+            FullHouse,
+            SmStraight,
+            LgStraight,
+            Yahtzee,
+            Chance
+        }
+    }
+
+    private Upper upper;
+    private Lower lower;
 }
